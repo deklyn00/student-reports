593cc2b79c4744f341af78b8338ea90feac538ae
Deklyn Lochner <deklyn.keith@gmail.com>
2024-03-23 07:00:18 +0000

    Adding database url to requirements.txt

--- grade_report/settings.py
+++ grade_report/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'grade_report.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url(os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
